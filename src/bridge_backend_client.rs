8f33f9282036a8f4b78074068cb5e2fdae22a071
Ceyhun Şen <ceyhuusen@gmail.com>
2024-10-07 10:59:24 +0300

    bridge_backend_client: Fix unused vars.

--- src/bridge_backend_client.rs
+++ src/bridge_backend_client.rs
@@ -5,11 +5,9 @@ use jsonrpsee::{
     ws_client::{PingConfig, WsClient, WsClientBuilder},
 };
 
-pub const MAX_FEE_PER_GAS: u128 = 1000000001;
-
 pub struct BridgeBackendClient {
-    http_client: HttpClient,
-    ws_client: WsClient,
+    _http_client: HttpClient,
+    _ws_client: WsClient,
     pub rpc_addr: SocketAddr,
 }
 
@@ -18,18 +16,18 @@ impl BridgeBackendClient {
         let http_host = format!("http://localhost:{}", rpc_addr.port());
         let ws_host = format!("ws://localhost:{}", rpc_addr.port());
 
-        let http_client = HttpClientBuilder::default()
+        let _http_client = HttpClientBuilder::default()
             .request_timeout(Duration::from_secs(120))
             .build(http_host)?;
 
-        let ws_client = WsClientBuilder::default()
+        let _ws_client = WsClientBuilder::default()
             .enable_ws_ping(PingConfig::default().inactive_limit(Duration::from_secs(10)))
             .build(ws_host)
             .await?;
 
         Ok(Self {
-            ws_client,
-            http_client,
+            _ws_client,
+            _http_client,
             rpc_addr,
         })
     }
