73eefe66a03a03347cc15ddf20d076efcf331835
jfldde <168934971+jfldde@users.noreply.github.com>
2025-05-13 13:04:01 +0100

    Clippy

--- src/config/test_case.rs
+++ src/config/test_case.rs
@@ -81,7 +81,7 @@ impl Default for TestCaseConfig {
                     |_| {
                         TempDir::new()
                             .expect("Failed to create temporary directory")
-                            .into_path()
+                            .keep()
                     },
                     PathBuf::from,
                 )
