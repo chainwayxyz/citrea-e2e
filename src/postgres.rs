f742009923c610ebeda3ac09284ff6b214c47e21
Mehmet Efe Akça <efeakca@gmail.com>
2025-07-21 15:13:38 +0300

    fix fmt

--- src/postgres.rs
+++ src/postgres.rs
@@ -2,7 +2,7 @@ use std::{sync::Arc, time::Duration};
 
 use anyhow::{bail, Context, Result};
 use async_trait::async_trait;
-use tracing::{debug};
+use tracing::debug;
 
 use crate::{
     config::PostgresConfig,
