200251df3beb50c9525279a0dfa66d60bb6c7452
Mehmet Efe Akça <efeakca@gmail.com>
2025-07-22 00:12:05 +0300

    fix fmt

--- src/clementine/mod.rs
+++ src/clementine/mod.rs
@@ -321,7 +321,6 @@ impl ClementineCluster {
         config: &ClementineClusterConfig,
         docker: Arc<Option<DockerEnv>>,
     ) -> Result<Self> {
-
         // Setup databases for clementine nodes
         setup_clementine_databases(&config.verifiers, &config.operators).await?;
 
