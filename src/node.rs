edc4c86bfa933e75f90d3bd66d3589f5f7dab85d
jfldde <168934971+jfldde@users.noreply.github.com>
2025-08-06 08:24:48 +0100

    Remove NodeCluster take method (#122)
    * Remove take lest it prevent shutdown
    
    * Add iter_mut method

--- src/node.rs
+++ src/node.rs
@@ -383,15 +383,6 @@ where
         self.inner.get(index)
     }
 
-    pub fn take(mut self, index: usize) -> (Self, Option<Node<C>>) {
-        if index < self.inner.len() {
-            let node = Some(self.inner.remove(index));
-            (self, node)
-        } else {
-            (self, None)
-        }
-    }
-
     #[allow(unused)]
     pub fn get_mut(&mut self, index: usize) -> Option<&mut Node<C>> {
         self.inner.get_mut(index)
@@ -399,6 +390,10 @@ where
 
     pub fn iter(&self) -> std::slice::Iter<'_, Node<C>> {
         self.inner.iter()
+    }
+
+    pub fn iter_mut(&mut self) -> std::slice::IterMut<'_, Node<C>> {
+        self.inner.iter_mut()
     }
 }
 
