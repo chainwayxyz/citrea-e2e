0a5ac009164d3f256c5f3b5837cc1b48ed6f4419
Ege Okan Ünaldı <egeokanunaldi@hotmail.com>
2025-10-23 15:45:14 +0300

    lint

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -33,11 +33,12 @@ pub use crate::citrea_config::{
     batch_prover::{BatchProverConfig, ProverGuestRunConfig},
     bitcoin::BitcoinServiceConfig,
     light_client_prover::LightClientProverConfig,
-    rollup::{RollupConfig, RollupPublicKeys, RpcConfig, RunnerConfig, StorageConfig},
     risc0::{
-        Risc0HostConfig, Risc0ProverConfig, BonsaiProverConfig, BoundlessConfig, LocalProverConfig, 
-        BoundlessPinataStorageConfig, BoundlessProverConfig, BoundlessS3StorageConfig, BoundlessStorageConfig
+        BonsaiProverConfig, BoundlessConfig, BoundlessPinataStorageConfig, BoundlessProverConfig,
+        BoundlessS3StorageConfig, BoundlessStorageConfig, LocalProverConfig, Risc0HostConfig,
+        Risc0ProverConfig,
     },
+    rollup::{RollupConfig, RollupPublicKeys, RpcConfig, RunnerConfig, StorageConfig},
     sequencer::{ListenModeConfig, SequencerConfig, SequencerMempoolConfig},
 };
 use crate::{log_provider::LogPathProvider, node::NodeKind, Result};
